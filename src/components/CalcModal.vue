8e76dad0527fda562106f73737251ca8cf1cb6ea
aleksandrbarancuk <academws1@gmail.com>
2025-07-22 20:35:10 +0300

    fix: updated base style

--- src/components/CalcModal.vue
+++ src/components/CalcModal.vue
@@ -1,6 +1,6 @@
 <template>
   <Teleport to="body">
-    <div tabindex="-1" aria-hidden="true" class="text-white overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full flex items-center justify-center">
+    <div tabindex="-1" aria-hidden="true" class="text-white overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-[calc(100%-1rem)] max-h-full flex items-center justify-center p-4">
       <div class="relative p-4 w-full max-w-2xl max-h-full">
         <!-- Modal content -->
         <div class="relative rounded-lg shadow-sm bg-gray-700">
